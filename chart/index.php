69b15e73ade5eb32e8346088b207c0a1265d1404
erngkung01 <arcanaarcana1@gmail.com>
2018-12-08 22:14:42 +0700

    update 8/12/2018

---
+++ chart/index.php
@@ -0,0 +1,93 @@
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "111,222,333,444";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "../index.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ระบบรายงานข้อมูลนักเรียน</title>
+<link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<?php include ("../head.php");?>
+
+<div class="container">
+<h1>ระบบรายงานข้อมูลนักเรียนโรงเรียนอินทารามวิทยา</h1>
+
+<div role="tabpanel">
+  <ul class="nav nav-tabs" role="tablist">
+    <li class="active"><a href="#home1" data-toggle="tab" role="tab">จำนวนนักเรียนรวม</a></li>
+    <li><a href="#paneTwo1" data-toggle="tab" role="tab">จำนวนนักเรียนแต่ละระดับชั้น</a></li>
+    
+  </ul>
+  <div id="tabContent1" class="tab-content">
+    <div class="tab-pane fade in active" id="home1">
+     <iframe src="all.php" width="100%" height="600"></iframe>
+   
+    </div>
+    <div class="tab-pane fade" id="paneTwo1">
+     <iframe src="group.php" width="100%" height="600"></iframe>
+    
+     
+    </div>
+   
+  </div>
+</div>
+<script src="../js/jquery-1.11.2.min.js" type="text/javascript"></script>
+<script src="../js/bootstrap.js" type="text/javascript"></script>
+
+
+</div>
+</body>
+</html>
