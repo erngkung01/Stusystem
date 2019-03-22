598751bf066172876ed838b83c9484752859e721
erngkung01 <arcanaarcana1@gmail.com>
2019-03-22 19:11:51 +0700

    update 22/3/2562

--- user/search.php
+++ user/search.php
@@ -145,10 +145,7 @@ $queryString_user = sprintf("&totalRows_user=%d%s", $totalRows_user, $queryStrin
 <head>
 <meta charset="utf-8">
 <title>ระบบจัดการข้อมูลสมาชิก</title>
-<link rel="stylesheet" href="../css/bootstrap.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
