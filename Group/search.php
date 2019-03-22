01b31cb9b1ad3d7a957ae52d7e2085dae3129402
erngkung01 <arcanaarcana1@gmail.com>
2019-03-22 19:04:03 +0700

    update 22/3/2562

--- Group/search.php
+++ Group/search.php
@@ -144,9 +144,7 @@ $queryString_group = sprintf("&totalRows_group=%d%s", $totalRows_group, $querySt
 <meta charset="utf-8">
 <title>ระบบจัดการข้อมูลนักเรียน</title>
 <link rel="stylesheet" href="../css/bootstrap.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
