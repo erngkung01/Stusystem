556fea9a7ea088d2c395568a7c77eea14ee9b4a4
erngkung01 <arcanaarcana1@gmail.com>
2019-03-22 18:33:50 +0700

    update 22/32562

--- head.php
+++ head.php
@@ -5,7 +5,9 @@
 <script type="text/javascript" src="<? echo $web; ?>/js/bootstrap.js"></script>
 <script type="text/javascript" src="<? echo $web; ?>/dist/js/bootstrap.min.js"></script>
 
-
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
